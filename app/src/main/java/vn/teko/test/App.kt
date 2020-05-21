553d60f69ac6505b6a888a9aab3903dc127e863a
giangdinh <giangdinh96@gmail.com>
2020-05-21 14:07:56 +0700

    Enable multidex

--- app/src/main/java/vn/teko/test/App.kt
+++ app/src/main/java/vn/teko/test/App.kt
@@ -1,10 +1,17 @@
 package vn.teko.test
 
+import android.content.Context
+import androidx.multidex.MultiDex
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import vn.teko.test.di.component.DaggerAppComponent
 
 class App : DaggerApplication() {
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.factory().create(this)
