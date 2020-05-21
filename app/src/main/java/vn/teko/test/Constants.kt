e5813a89548ca533cdf539a5b8b1b3afd6c1dde8
giangdinh <giangdinh96@gmail.com>
2020-05-21 18:37:38 +0700

    add constants

---
+++ app/src/main/java/vn/teko/test/Constants.kt
@@ -0,0 +1,21 @@
+package vn.teko.test
+
+const val BASE_ACTIVITY_LAYOUT_ID = R.layout.activity_base
+const val FRAGMENT_CONTAINER_ID = R.id.container_id
+
+@JvmField
+val COLOR_REFRESH_PROGRESS = intArrayOf(
+    android.R.color.holo_red_light,
+    android.R.color.holo_blue_light,
+    android.R.color.holo_green_light,
+    android.R.color.holo_orange_light
+)
+
+const val FONT_DEFAULT_PATH = "fonts/SFProText-Regular.ttf"
+
+// Anim
+const val ANIM_ENTER_RIGHT_TO_LEFT = R.anim.anim_enter_right_to_left
+const val ANIM_EXIT_RIGHT_TO_LEFT = R.anim.anim_exit_right_to_left
+const val ANIM_ENTER_LEFT_TO_RIGHT = R.anim.anim_enter_left_to_right
+const val ANIM_EXIT_LEFT_TO_RIGHT = R.anim.anim_exit_left_to_right
+const val ANIM_NONE = R.anim.anim_none
