98b1427a741455c615029f5e989b0a587eb1232b
giangdinh <giangdinh96@gmail.com>
2020-05-22 22:02:47 +0700

    add StringExt

---
+++ app/src/main/java/vn/teko/test/extension/StringExt.kt
@@ -0,0 +1,28 @@
+package vn.teko.test.extension
+
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.RelativeSizeSpan
+import android.text.style.StrikethroughSpan
+import android.text.style.SuperscriptSpan
+
+fun String.toStrikethroughSpanString(): Spannable {
+    val result = SpannableString(this)
+    val span = StrikethroughSpan()
+    result.setSpan(span, 0, result.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    return result
+}
+
+fun String.toLastLetterSuperscriptSpanString(): Spannable {
+    val result = SpannableString(this)
+    val span = SuperscriptSpan()
+    val spanTextSize = RelativeSizeSpan(0.5f)
+    result.setSpan(span, result.length - 1, result.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    result.setSpan(
+        spanTextSize,
+        result.length - 1,
+        result.length,
+        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
+    return result
+}
